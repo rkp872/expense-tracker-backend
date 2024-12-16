d8ef643c72f53759c7ddaba72a85a612ec3f1ee0
Rohit Pandey <pandeyrohit872@gmail.com>
2024-12-16 14:28:46 +0530

    added pagenation & filtering

--- src/main/java/com/expense/tracker/repositories/ExpenseRepository.java
+++ src/main/java/com/expense/tracker/repositories/ExpenseRepository.java
@@ -1,5 +1,9 @@
 package com.expense.tracker.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,15 @@ import com.expense.tracker.models.Expense;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    // Query to filter by category and date range
+    Page<Expense> findByCategoryAndDateBetween(String category,LocalDate startDate,LocalDate endDate, Pageable pageable);
 
+     // Query to filter by category
+     Page<Expense> findByCategory(String category, Pageable pageable);
+
+     // Query to filter by date range
+     Page<Expense> findByDateBetween(LocalDate startDate,LocalDate endDate, Pageable pageable);
+
+     // Get all expenses with pagenation
+     Page<Expense> findAll(Pageable pageable);
 }
